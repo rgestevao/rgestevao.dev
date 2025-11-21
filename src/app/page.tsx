817562fce1cbde4ce01a2f60b50eeab841ec19cf
rodrigoges <rgestevao@gmail.com>
2025-11-21 15:09:39 -0300

    feat: starting the ptoject

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<main>
+			<header>
+				<h1>Rodrigo Gouveia Estevão</h1>
+				<span>Software Developer</span>
+				<span>Dark</span>
+				<span>English</span>
+			</header>
+		</main>
+	)
+}
